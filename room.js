af2c45c4e13ad5aa1f743ae2627ffc3f031f0b89
vanyagoyal <73106041+vanyagoyal@users.noreply.github.com>
2021-01-23 20:25:48 +0530

    Add files via upload

---
+++ room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDiBNh9cqnYFNggU1kgMsiP9xYAUTl7dAs",
+    authDomain: "lets-chat-a2a19.firebaseapp.com",
+    databaseURL: "https://lets-chat-a2a19-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-a2a19",
+    storageBucket: "lets-chat-a2a19.appspot.com",
+    messagingSenderId: "624739082101",
+    appId: "1:624739082101:web:4179bc94227f2071ef3d3c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
